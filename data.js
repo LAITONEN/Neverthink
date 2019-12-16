96ad6d610a379f623d88c32ecada97a83a330812
Eduard Rastoropov <eduard@rastoropov@gmail.com>
2019-12-17 00:30:45 +0200

    Added data

---
+++ data.js
@@ -0,0 +1,22 @@
+export const channels = [ {
+    id: 1,
+    name: 'Meme Radar', icon:
+    'https://neverthink.tv/assets/images/63e3939725b3d92af5e7b8429a0f4d57e6be661abf380b39348f360e528dd6e2.png',
+    playlist: ['QSqIG5Dl-SM', 'Km8kIX-8hVs'],
+    nextVideoIndex: 0,
+  },
+  {
+    id: 2,
+    name: 'LOL',
+    icon: 'https://neverthink.tv/assets/images/61d1aeee19fd7cff13a8b17727f1b5a4e9645f16c42ff376a5e5f3ce8a373df2.png',
+    playlist: ['_Czxy3nya8Y', 'p8UR4dODogI', 'HoL1csZPYsk', 'ADrBo7u3tR4'],
+    nextVideoIndex: 0,
+  }, 
+  {
+    id: 3,
+    name: 'WTF', icon:
+    'https://neverthink.tv/assets/images/fde01ee47dc02d83892c35c22f2efd81f52c37edc4f3651be40094a115c812fd.png',
+    playlist: ['Y46GXMD3ZXQ', 'ErfEnD2WA3A', 'JZnlJ2upJv8', 'Km8kIX-8hVs'],
+    nextVideoIndex: 0,
+  } 
+]
